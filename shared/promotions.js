c74fe6aa21ee48e8775da3f0a0d2cefcbd950ebd
Nick Spera <nickaspera@gmail.com>
2020-04-30 18:29:12 -0400

    Added Assets

--- shared/promotions.js
+++ shared/promotions.js
@@ -2,7 +2,7 @@ export const PROMOTIONS = [
     {
         id: 0,
         name: "Mountain Adventure",
-        image: "assets/images/massage001.jpg",
+        image: "assets/images/promotion_01.jpg",
         featured: true,
         description: "Book a 5-day mountain trek with a seasoned outdoor guide! Fly fishing equiptment and lessons provided."
     }
--- shared/services.js
+++ shared/services.js
@@ -2,24 +2,24 @@ export const SERVICES = [
     {
         id: 0,
         name: "Massage",
-        image: "/assets/images/massage001.jpg",
+        image: "/assets/images/massage-service.jpg",
         featured: true,
         description: "Massage description in shared/services.js"
     },
-    // {
-    //     id: 1,
-    //     name: "BioEnergetic Stimulation Technique",
-    //     image: "/assets/images/massage001.jpg",
-    //     featured: false,
-    //     description: "BioEnergetic Stimulation description in shared/services.js"
-    // },
-    // {
-    //     id: 2,
-    //     name: "CranioSacral Therapy",
-    //     image: "/assets/images/massage001.jpg",
-    //     featured: false,
-    //     description: "CranioSacral Therapy description in shared/services.js"
-    // }
+    {
+        id: 1,
+        name: "BioEnergetic Stimulation Technique",
+        image: "/assets/images/biosync-service.jpg",
+        featured: false,
+        description: "BioEnergetic Stimulation description in shared/services.js"
+    },
+    {
+        id: 2,
+        name: "CranioSacral Therapy",
+        image: "/assets/images/craniosacral-service.jpg",
+        featured: false,
+        description: "CranioSacral Therapy description in shared/services.js"
+    }
 ];
 
 // Edit components/BodyworkComponent.js so only the services with feature true are displayed on this page (commented out in shared/services.js)
